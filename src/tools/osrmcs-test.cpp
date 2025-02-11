ce93dc00631d68385071349c11b770c9a8ad33eb
Cillie Malan <CillieM@eurekadiy.co.za>
2025-02-11 14:31:39 +0200

    vrooooooom

--- src/tools/osrmcs-test.cpp
+++ src/tools/osrmcs-test.cpp
@@ -2,7 +2,131 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define IMPLEMENTATION 0
 #include "osrmcs.hpp"
+
+const char vroomreq[] = R"END(
+{
+  "vehicles": [
+    {
+      "id": 1,
+      "start": [27.801654, -25.648911],
+      "end": [27.801654, -25.648911],
+      "capacity": [
+        4
+      ],
+      "skills": [
+        1,
+        14
+      ],
+      "time_window": [
+        1600416000,
+        1600430400
+      ]
+    },
+    {
+      "id": 2,
+      "start": [27.801654, -25.648911],
+      "end": [27.801654, -25.648911],
+      "capacity": [
+        4
+      ],
+      "skills": [
+        2,
+        14
+      ],
+      "time_window": [
+        1600416000,
+        1600430400
+      ],
+      "breaks": [
+        {
+          "id": 2,
+          "service": 300,
+          "time_windows": [
+            [
+              1600423200,
+              1600425000
+            ]
+          ]
+        }
+      ]
+    }
+  ],
+  "jobs": [
+    {
+      "id": 1,
+      "service": 300,
+      "delivery": [
+        1
+      ],
+      "location": [27.782810, -25.638839],
+      "skills": [
+        1
+      ],
+      "time_windows": [
+        [
+          1600419600,
+          1600423200
+        ]
+      ]
+    },
+    {
+      "id": 2,
+      "service": 300,
+      "pickup": [
+        1
+      ],
+      "location": [27.782450, -25.631061],
+      "skills": [
+        1
+      ]
+    },
+    {
+      "id": 5,
+      "service": 300,
+      "delivery": [
+        1
+      ],
+      "location": [27.794629, -25.622724],
+      "skills": [
+        14
+      ]
+    },
+    {
+      "id": 6,
+      "service": 300,
+      "delivery": [
+        1
+      ],
+      "location": [27.783958, -25.638316],
+      "skills": [
+        14
+      ]
+    }
+  ],
+  "shipments": [
+    {
+      "amount": [
+        1
+      ],
+      "skills": [
+        2
+      ],
+      "pickup": {
+        "id": 4,
+        "service": 300,
+        "location": [27.779071, -25.641201]
+      },
+      "delivery": {
+        "id": 3,
+        "service": 300,
+        "location": [27.793146, -25.719918]
+      }
+    }
+  ]
+}
+)END";
 
 int main(int argc, const char **argv)
 {
@@ -23,9 +147,10 @@ int main(int argc, const char **argv)
         {11.980590820312502, 51.37863823622007},
     };
 
-    auto route = osrmcs_optimize(
-        instance, coordinates, sizeof(coordinates) / sizeof(coordinates[0]), true, false, false);
-    osrmcs_delete(route);
+    osrmcs_optimize_advanced(instance, vroomreq);
+
+    //auto route = osrmcs_optimize(instance, coordinates, sizeof(coordinates) / sizeof(coordinates[0]), true, false, false);
+    //osrmcs_delete(route);
     osrmcs_delete_instance(instance);
 
     return 0;
